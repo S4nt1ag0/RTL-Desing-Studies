686db50f590ab8400b1f25e9a94b4d66650a4688
S4nt1ag0 <gustavosantiago1018@gmail.com>
2025-02-18 09:17:23 -0300

    lab04 scale_mux

---
+++ labs/lab04__scale_mux/rtl/scale_mux.sv
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/17/2025 04:47:11 PM
+// Design Name: 
+// Module Name: register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module scale_mux(
+    input logic [7:0] in_a, 
+    input logic [7:0] in_b,
+    input logic sel_a,
+    output logic [7:0] out
+    );
+
+    always_comb begin: select_data
+        if(!sel_a) begin
+            out <= in_b;
+        end else begin
+            out <= in_a;
+        end
+    end
+
+endmodule
---
+++ labs/lab04__scale_mux/tb/scale_mux_tb.sv
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/10/2025 01:38:28 PM
+// Design Name: 
+// Module Name: arithmetic_unit_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module scale_mux_tb();
+
+   
+    logic [7:0] in_a;
+    logic [7:0] in_b;
+    logic sel_a;
+    logic [7:0] out;
+    
+    scale_mux duv (
+        .in_a(in_a),
+        .in_b(in_b),
+        .sel_a(sel_a),
+        .out(out)
+    );
+
+    initial begin
+        in_a = 8'h0; in_b = 8'h0; sel_a = '0;
+        #1 
+        in_a = 8'h0; in_b = 8'h0; sel_a = '1;
+        #1
+        in_a = 8'h0; in_b = 8'hff; sel_a = '0;
+        #1
+        in_a = 8'h0; in_b = 8'hff; sel_a = '1;
+        #1
+        in_a = 8'hff; in_b = 8'h00; sel_a = '0;
+        #1
+        in_a = 8'hff; in_b = 8'h00; sel_a = '1;
+        #1
+        in_a = 8'hff; in_b = 8'hff; sel_a = '0;
+        #1
+        in_a = 8'hff; in_b = 8'hff; sel_a = '1;
+        #1
+        // End of simulation
+        $finish;
+    end
+endmodule
