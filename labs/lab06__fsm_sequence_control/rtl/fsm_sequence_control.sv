466fa25a69363cf7fd7f68497100d9b88e45a7ab
S4nt1ag0 <gustavosantiago1018@gmail.com>
2025-02-18 17:24:43 -0300

    lab06

---
+++ labs/lab06__fsm_sequence_control/rtl/fsm_sequence_control.sv
@@ -0,0 +1,133 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/17/2025 04:47:11 PM
+// Design Name: 
+// Module Name: fsm_calculator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module fsm_sequence_control
+    import typedefs::*;
+#(
+    parameter WORD_WIDTH = 8
+)
+(
+    input opcodes_t opcode ,
+    input logic rst_n,
+    input logic clk,
+    input logic zero,
+    output logic mem_rd,
+    output logic load_ir,
+    output logic halt,
+    output logic inc_pc,
+    output logic load_ac,
+    output logic load_pc,
+    output logic mem_wr
+    );
+
+    state_t state = INST_ADDR, next_state;
+
+    always_comb begin: process_next_state
+        next_state = state;
+        case (state)
+            INST_ADDR: next_state = INST_FETCH;
+            INST_FETCH: next_state = INST_LOAD;
+            INST_LOAD: next_state = IDLE;
+            IDLE: next_state = OP_ADDR;
+            OP_ADDR: next_state = OP_FETCH;
+            OP_FETCH: next_state = ALU_OP;
+            ALU_OP: next_state = STORE;
+            STORE: next_state = INST_ADDR;
+        endcase  
+    end
+
+    always_ff @(posedge clk, negedge rst_n) begin : proc_stage
+        if(!rst_n) begin
+            state <= INST_ADDR;
+        end else begin 
+            state <= next_state;
+        end
+    end
+
+    
+
+    always_comb begin: state_decode
+        mem_rd = 1'b0;
+        load_ir = 1'b0;
+        halt = 1'b0;
+        inc_pc = 1'b0;
+        load_ac = 1'b0;
+        load_pc = 1'b0;
+        mem_wr = 1'b0;
+
+        case (state)
+            INST_ADDR: begin
+                mem_rd = 1'b0;
+                load_ir = 1'b0;
+                halt = 1'b0;
+                inc_pc = 1'b0;
+                load_ac = 1'b0;
+                load_pc = 1'b0;
+                mem_wr = 1'b0;
+            end
+            INST_FETCH: begin
+                mem_rd = 1'b1;
+            end
+            INST_LOAD: begin
+                mem_rd = 1'b1;
+                load_ir = 1'b1;
+            end
+            IDLE: begin
+                mem_rd = 1'b1;
+                load_ir = 1'b1;
+            end
+            OP_ADDR: begin
+                inc_pc = 1'b1;
+                if(opcode == HLT)
+                    halt = 1'b1;
+            end
+            OP_FETCH: begin
+                if(opcode == ADD || opcode == AND || opcode == XOR || opcode == LDA)
+                    mem_rd = 1'b1;
+            end
+            ALU_OP: begin
+                if(opcode == ADD || opcode == AND || opcode == XOR || opcode == LDA) begin
+                    mem_rd = 1'b1;
+                    load_ac = 1'b1;
+                end
+                else if( opcode == SKZ && zero)
+                    inc_pc = 1'b1;
+                else if(opcode == JMP)
+                    load_pc = 1'b1; 
+            end
+            STORE: begin
+                if(opcode == ADD || opcode == AND || opcode == XOR || opcode == LDA) begin
+                    mem_rd = 1'b1;
+                    load_ac = 1'b1;
+                end
+                else if(opcode == JMP) begin
+                    inc_pc = 1'b1;
+                    load_pc = 1'b1;
+                end
+                else if(opcode == STO)
+                    mem_wr = 1'b1;
+            end
+        endcase  
+    end
+
+    
+
+endmodule
---
+++ labs/lab06__fsm_sequence_control/rtl/typedefs.sv
@@ -0,0 +1,29 @@
+package typedefs;
+    parameter VERSION = "1.1";
+    localparam DEFAULT_WORD_W = 8;
+    localparam OPCODE_WITH = 8;
+    localparam STATE_WITH = 8;
+   
+    typedef enum logic [OPCODE_WITH-1:0] {
+        HLT, //halt
+        SKZ, //skip if zero
+        ADD, //data + acumulator
+        AND, //data & acumulator
+        XOR, //data ^ acumulator
+        LDA, //load acumulator
+        STO, //store acumulator
+        JMP //Jump to address
+        } opcodes_t;
+
+        typedef enum logic [STATE_WITH-1:0] {
+        INST_ADDR,
+        INST_FETCH,
+        INST_LOAD,
+        IDLE,
+        OP_ADDR,
+        OP_FETCH,
+        ALU_OP,
+        STORE
+        } state_t;
+
+endpackage
