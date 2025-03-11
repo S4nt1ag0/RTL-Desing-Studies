031f65eed1d0fdc29f32ff87aa6753350ebcd84c
S4nt1ag0 <gustavosantiago1018@gmail.com>
2025-03-11 15:12:05 -0300

    try interface with class

---
+++ labs/lab13__memclass/rtl/memory_module_intf.sv
@@ -0,0 +1,65 @@
+interface memory_module_intf
+#(parameter ADDR_WIDTH = 5, parameter DATA_WIDTH = 8, parameter debug = 0)
+(
+    input clk
+);
+
+    logic read;
+    logic write;
+    logic [ADDR_WIDTH-1:0] addr;
+    logic [DATA_WIDTH-1:0] data_in;
+    logic [DATA_WIDTH-1:0] data_out;
+    
+    task automatic write_mem(
+        input [ADDR_WIDTH-1:0] in_addr,
+        input [DATA_WIDTH-1:0] in_data
+    );
+        @(negedge clk);
+        write <= 1'b1;
+        read  <= 1'b0;
+        addr  <= in_addr;
+        data_in <= in_data;
+        @(negedge clk);
+        write <= 1'b0;
+        if (debug)
+            $display("Write Data | Address = %d, Data = %h", in_addr, in_data);
+    endtask: write_mem
+
+    task automatic read_mem(
+        input [ADDR_WIDTH-1:0] in_addr,
+        output [DATA_WIDTH-1:0] out_data
+    );
+        @(negedge clk);
+        write <= 1'b0;
+        read  <= 1'b1;
+        addr  <= in_addr;
+        @(negedge clk);
+        out_data = data_out;
+        read <= 1'b0;
+        @(negedge clk); //Extra delay to ensure the read value is returned
+        if (debug)
+            $display("Read Data | Address = %d, Data = %h", in_addr, out_data);
+    endtask: read_mem
+
+    // From TestBench perspective
+    modport TB  (
+        input clk,
+        output read,
+        output write,
+        output addr,
+        output data_in,
+        input  data_out,
+        import write_mem,
+        import read_mem
+    );
+
+    // From DUT perspective
+    modport DUT (
+        input clk,
+        input read,
+        input write,
+        input addr,
+        input data_in,
+        output data_out
+    );
+endinterface
---
+++ labs/lab13__memclass/tb/memclass_tb.sv
@@ -0,0 +1,59 @@
+`timescale 1ns/100ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/10/2025 01:34:33 PM
+// Design Name: 
+// Module Name: memory_module_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module memclass_tb();
+    localparam ADDR_WIDTH = 5;
+    localparam DATA_WIDTH = 8;
+    localparam CLK_PERIOD = 5ns;
+    localparam debug = 0;
+
+    logic clk;
+    
+    // Instanciando a interface
+    memory_module_intf #(.ADDR_WIDTH (ADDR_WIDTH), .DATA_WIDTH (DATA_WIDTH), .debug(debug)) memoryIntf (clk);
+
+    // Instanciando o DUT
+    memory_module #(.ADDR_WIDTH (ADDR_WIDTH), .DATA_WIDTH (DATA_WIDTH)) dut (memoryIntf.DUT);
+
+    // Instanciando o módulo de teste
+    mem_test #(.ADDR_WIDTH (ADDR_WIDTH), .DATA_WIDTH (DATA_WIDTH)) tb(memoryIntf.TB);
+
+    // Geração do clock
+    initial begin
+        clk = 1;
+        forever begin
+            #CLK_PERIOD clk = ~clk;
+        end
+    end
+
+    // Testbench principal
+    memTest test; // Declarando a variável sem instanciar
+
+    initial begin
+        $dumpfile("lab_13.vcd");
+        $dumpvars(0, memclass_tb);
+
+        // Fazendo a instância da classe separadamente
+        test = new(memoryIntf, memTest::constraint_mode_t'(memTest::MIXED));
+        test.run_tests();
+    end
+
+endmodule
